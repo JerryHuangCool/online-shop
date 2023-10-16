62834ae85a3c01a91d4d8e387a7b6f2a0f25a97a
清茶团子 <905056895@qq.com>
2023-10-16 17:22:35 +0800

    全局配置

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    'postcss-px-to-viewport': {
+      // 标准屏幕宽度跟设计图有关
+      viewportWidth: 375
+    }
+  }
+}
---
+++ src/utils/vant-ui.js
@@ -0,0 +1,4 @@
+import { Button } from 'vant'
+import Vue from 'vue'
+
+Vue.use(Button)
